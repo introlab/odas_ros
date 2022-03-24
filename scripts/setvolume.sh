2ea033c3b6d9990626a68102806dd52bd3e0dccd
philippewarren <philippewarren31@gmail.com>
2022-03-24 16:22:03 -0400

    Update setvolume script

--- scripts/setvolume.sh
+++ scripts/setvolume.sh
@@ -5,10 +5,16 @@
 # $1 is the sound source name from "pacmd list-sources | grep 'name:'" without the angle brackets <>
 
 max=$(pactl list sources | perl -n0777E "say \$1 if /^.*Name: $1.*?Base Volume: ([0-9]+).*/s")
+
+if [ -z "$max" ]; then
+    echo "Error: Could not find source $1" 1>&2
+    exit 1
+fi
+
 value=$(($2 * $max / 100))
 
 if [ -z "$value" ]; then
-    echo "Error: Could not find source $1"
+    echo "Error: Could not find source $1" 1>&2
     exit 1
 fi
 
